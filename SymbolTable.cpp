ca3962cbf7711cf7dcfbbabad8d52130d8042c5a
hnamng2301 <meomaythongminh3@gmail.com>
2021-08-27 09:24:20 +0700

    First change

--- SymbolTable.cpp
+++ SymbolTable.cpp
@@ -1,5 +1,38 @@
 #include "SymbolTable.h"
 
 void SymbolTable::run(string filename) {
+    ifstream file(filename);
+    string tmp = "";
+
+    if(file.is_open()){
+        //getline(file, tmp);
+        while(getline(file, tmp)){
+            regex reg("\\s+");
+            sregex_token_iterator iter(tmp.begin(), tmp.end(), reg, -1);
+            sregex_token_iterator end;
+            vector<string> vec(iter, end);
+            if(vec[0] == "INSERT"){
+                insert(vec[0],vec[1]);
+            }
+            else if(vec[0] == "ASSIGN"){
+
+            }
+            else if(vec[0] == "LOOKUP"){
+
+            }
+            else if(vec[0] == "PRINT"){
+
+            }
+            else if(vec[0] == "RPRINT"){
+
+            }
+        }
+    }
     cout << "success";
+}
+
+void SymbolTable::insert(string name, string type){
+    Node *p = new Node(name, type);
+
+    
 }
--- SymbolTable.h
+++ SymbolTable.h
@@ -2,9 +2,43 @@
 #define SYMBOLTABLE_H
 #include "main.h"
 //Test change
+
+class Node // khai bao Node
+{
+    string identifier, dataType, scope;
+    int lineNum;
+    Node *next;
+
+public:
+    Node(){
+        next = nullptr;
+    }
+    
+    Node(string name, string type){
+        identifier = name;
+        dataType = type;
+        next = nullptr;
+    }
+
+    friend class SymbolTable;
+};
+
+class Table{
+public:
+    Table(): head(NULL) {}
+    Table(Node *head){}
+    Node *head;
+};
+
 class SymbolTable {
 public:
     SymbolTable() {}
-    void run(string filename);
+    void run(string filename); //default
+    void insert(string name, string type);
+    void assign();
+    void scope();
+    void lookUp();
+    void print();
+    void rPrint();
 };
 #endif
